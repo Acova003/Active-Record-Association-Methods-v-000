effe409a74f8a6464f44d9bc4b1d1dbb0c98276a
Amee Covarrubias <ameecova@yahoo.com>
2018-11-28 04:54:03 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -15,7 +15,7 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    artists = self.artists
+    self.artists.map {||}
     binding.pry
   end
 end
